ca8b0cda057be8d55cb065261f6aa59a5e7b81f3
TanishqDsharma <tanishqsharma612@gmail.com>
2025-02-04 22:43:14 +0530

    Added library

---
+++ lib/Tick.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+
+library tick{
+    function tickSpacingToMaxLiquidityPerTick(int24 tickspacing) internal pure  returns (uint128){}
+}
