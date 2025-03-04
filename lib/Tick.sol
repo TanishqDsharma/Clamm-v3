baed2f1cc0a0557fe875f1af15d8ac4ce848bbc9
TanishqDsharma <tanishqsharma612@gmail.com>
2025-03-04 09:46:40 +0530

    Added new function in Tick library

--- lib/Tick.sol
+++ lib/Tick.sol
@@ -9,5 +9,26 @@ library tick{
     bool initialized;
     uint128 liquidity;
    }
+
+   function update(
+      mapping(int24=>tick.Info) storage self,
+      int24 Tick,
+      uint128 liquidityDelta
+   ) internal 
+   {
+      tick.Info storage tickInfo = self[Tick];
+      uint128 liquidityBefore= tickInfo.liquidity;
+      uint128 liquidityAfter= liquidityBefore + liquidityDelta;
+
+      if(liquidityBefore==0){
+         tickInfo.initialized = true;
+      }
+
+      tickInfo.liquidity=liquidityAfter;
+
+   }
+
+
+
 }
 
